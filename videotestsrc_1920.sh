cbcebcc88b2c8f92da736f581e042943182ee5a7
hhdrake <hans@git.com>
2024-07-24 13:39:56 +0000

    hans latest

--- videotestsrc_1920.sh
+++ videotestsrc_1920.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#gst-variable-rtsp-server -p 9003 -u "videotestsrc pattern=21 ! video/x-raw, width=1920, height=1080 ! gdkpixbufoverlay location=clipart333306.png positioning-mode=0 offset-x=572 offset-y=152 ! videoconvert ! vpuenc_h264 ! rtph264pay name=pay0 pt=96" &
-gst-variable-rtsp-server -p 9003 -u "videotestsrc pattern=25 ! video/x-raw, width=1920, height=1080 ! videoconvert ! vpuenc_h264 ! rtph264pay name=pay0 pt=96" &
+gst-variable-rtsp-server -p 9004 -u "videotestsrc ! video/x-raw, width=1920, height=1080 ! videoconvert ! vpuenc_h264 ! rtph264pay name=pay0 pt=96" &
