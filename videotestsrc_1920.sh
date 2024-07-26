1126393c85df1f3d9ff661b156592571310f7828
Kobus Goosen <KobusG@users.noreply.github.com>
2024-07-26 09:11:51 +0200

    cleanup

--- videotestsrc_1920.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gst-variable-rtsp-server -p 9004 -u "videotestsrc ! video/x-raw, width=1920, height=1080 ! videoconvert ! vpuenc_h264 ! rtph264pay name=pay0 pt=96" &
--- videotestsrc_640.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gst-variable-rtsp-server -p 9003 -u "videotestsrc ! video/x-raw, width=640, height=480 ! videoconvert ! vpuenc_h264 ! rtph264pay name=pay0 pt=96" &
