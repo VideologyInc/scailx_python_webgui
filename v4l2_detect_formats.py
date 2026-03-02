826896d8896ff23d7d798d4765478f8e4fb7849c
PingVideology <jye@videologyinc.com>
2026-03-02 21:10:50 +0000

    AB#2045. Added more ZoomBlock camera formats by visca commands to go2rtc. But it only works with MANUALL running 'vdlg-lvds-setres' command to set up resolution and high fps before running gst-launch-1.0 command ;-)

--- v4l2_detect_formats.py
+++ v4l2_detect_formats.py
@@ -4,7 +4,10 @@
 
 File:   v4l2_detect_formats.py
 
-Detect formats by parsing from v4l2 command v4l2-ctl -d /dev/video0 --list-formats-ext outputs.
+2026.0227.  Detect formats by parsing from v4l2 command v4l2-ctl -d /dev/video0 --list-formats-ext outputs.
+2026.0302.  Added Zoom Block camera format full list (resolution, fps, formats) from Visca commands.
+
+By:			jye@videologyinc.com
 
 """
 
@@ -13,9 +16,9 @@ import time
 import subprocess
 import re
 import json
-
 import glob
-
+import copy
+import math
 
 # Camera key words in device tree and its regular names
 camera_dict = {
@@ -117,6 +120,40 @@ def formats_filter_out_unwanted(format_list):
     
     return format_list_filtered
 
+# For Zoom Block cameras connecte to LVDS port, add fps = 25,30,50,60 and 1920 x 1080, plus 1280 x 720 to the list.
+def add_formats_lvds(format_list):
+    formats = {"YUYV" : "YUYV 4:2:2", "NV12" : "Y/UV 4:2:0"}
+    resolution_list = [(1920,1080), (1280, 720)]
+    fps_list = [25.0, 30.0, 50.0, 60.0]
+    # Add all combinations 2 x 2 x 4 = 16 total possible formats
+    sample = {
+        "pixelformat": "YUYV",
+        "description": "YUYV 4:2:2",
+        "sizes": [
+            {
+                "width": 1920,
+                "height": 1080,
+                "fps": [25.0]
+            }
+        ]
+    }
+
+    format_list = []
+    for f in formats.keys():
+        for w,h in resolution_list:
+            for fps in fps_list:
+
+                print(f, w, h, fps)
+                one = copy.deepcopy(sample)
+                one["pixelformat"] = f
+                one["description"] = formats[f]
+                one["sizes"][0]["width"] = w
+                one["sizes"][0]["height"] = h
+                one["sizes"][0]["fps"][0] = fps
+                format_list.append(one)
+
+    return format_list
+
 
 def fourcc_to_gst(fourcc_str):
     if fourcc_str in Fourcc_Dict:
@@ -128,6 +165,41 @@ def v4l2_format_mapto_gst(format_list):
     for fdict in format_list:
         print(fdict["pixelformat"], "=>", fourcc_to_gst(fdict["pixelformat"]))
 
+
+# Given one format dict, return its gstreamer string.
+def v4l2_format_to_gst(format_dict):
+    s_list = []
+    for sz in format_dict["sizes"]:
+        w = sz["width"]
+        h = sz["height"]
+        fps = int(math.ceil(sz["fps"][0]))
+        f = format_dict["pixelformat"]
+        f_gst = fourcc_to_gst(f)
+        s = f"video/x-raw,width={w},height={h},framerate={fps}/1,format={f_gst} ! videoconvert"
+        t = (w, h, f"fps={fps},format={f_gst}", s)
+        s_list.append(t)
+
+    return s_list
+
+# Given camera device path, return supported gstreamer str list.
+def camera_to_gst_list(device):
+    camera_type = detect_camera_type(device)
+
+    camera_formats = formats_filter_out_unwanted(parse_v4l2_formats(device))
+    # Add full resolution and framerate support for ZoomBlock (from visca commands)
+    print(camera_type)
+    if "zoomblock"==camera_type:
+        print("Create new format list for ZoomBlock cameras.")
+        camera_formats = add_formats_lvds(camera_formats)
+
+    # print(json.dumps(camera_formats, indent=2))
+
+    info_list = []
+    for fd in camera_formats:
+        s_list = v4l2_format_to_gst(fd)
+        info_list += s_list
+
+    return info_list
 
 # Example Usage
 if __name__ == "__main__":
@@ -142,11 +214,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    camera_type = detect_camera_type(args.device)
-    print(camera_type)
+    info_list = camera_to_gst_list(args.device)
 
-    camera_formats = formats_filter_out_unwanted(parse_v4l2_formats(args.device))
-    print(json.dumps(camera_formats, indent=2))
-
-    v4l2_format_mapto_gst(camera_formats)
+    for info in info_list:
+        print(info)
     
