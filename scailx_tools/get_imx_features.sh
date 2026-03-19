345ec688c1de02840defae7abf81df57369ebc55
PingVideology <jye@videologyinc.com>
2026-03-19 17:52:06 +0000

    AB#2097. Updated sh and tested with imx678 camera.

--- scailx_tools/get_imx_features.sh
+++ scailx_tools/get_imx_features.sh
@@ -1,47 +1,57 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 </dev/video id>"
+  exit 1
+fi
+
+echo "Camera device is /dev/video$1"
+
 echo Run vvget to get feature values of imx camera.
 
-echo
+echo ========================================
 echo FrameRate and Black Level Subtraction
 echo
-./vvget 0 'FPS'
-./vvget 0 'BLS'
+vvget $1 'FPS'
+vvget $1 'BLS'
 
-echo
+echo ============================================
 echo Gamma and Denoise PreFilter
 echo
-./vvget 0 'GAMMA ON/OFF'
-./vvget 0 'DPF ON/OFF'
+vvget $1 'GAMMA ON/OFF'
+vvget $1 'DPF ON/OFF'
 
-echo
+echo ========================================================
 echo Color Processing, Brightness, Contrast, Saturation, Hue
 echo
-./vvget 0 'CPROC ON/OFF'
-./vvget 0 'Adjust brightness'
-./vvget 0 'Adjust contrast'
-./vvget 0 'Adjust saturation'
-./vvget 0 'Adjust HUE'
+vvget $1 'CPROC ON/OFF'
+vvget $1 'Adjust brightness'
+vvget $1 'Adjust contrast'
+vvget $1 'Adjust saturation'
+vvget $1 'Adjust HUE'
 
-echo
+echo =================================================
 echo AEC, SetPoint, Gain, ExposureTime, etc.
 echo
-./vvget 0 'AEC On/Off'
-./vvget 0 'AEC SetPoint'
-./vvget 0 'AEC DampOver'
-./vvget 0 'AEC DampUnder'
-./vvget 0 'AEC Tolerance'
-./vvget 0 'AEC Gain'
-./vvget 0 'AEC ExposureTime'
-./vvget 0 'AEC Sensitivity'
+vvget $1 'AEC On/Off'
+vvget $1 'AEC SetPoint'
+vvget $1 'AEC DampOver'
+vvget $1 'AEC DampUnder'
+vvget $1 'AEC Tolerance'
+vvget $1 'AEC Gain'
+vvget $1 'AEC ExposureTime'
+vvget $1 'AEC Sensitivity'
 
-echo
+echo ==================================
 echo AWB, Gain, CCM, Offset, etc.
 echo
-./vvget 0 'AWB On/Off'
-./vvget 0 'AWB Auto Ctrl item'
-./vvget 0 'GAIN INPUT'
-./vvget 0 'CCM INPUT'
-./vvget 0 'Offset INPUT'
+vvget $1 'AWB On/Off'
+vvget $1 'AWB Auto Ctrl item'
+vvget $1 'GAIN INPUT'
+vvget $1 'CCM INPUT'
+vvget $1 'Offset INPUT'
 
+echo =====================================
 echo Finished
 
 
