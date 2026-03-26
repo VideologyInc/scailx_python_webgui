8e5b32121941236da2f5cb17ce8c79e65f4540de
PingVideology <jye@videologyinc.com>
2026-03-26 17:08:59 +0000

    AB#2123. Added bash script to do object detection at 640 x 512 x NV12 => Gray8 format to replace excluded 640 x 512 x Gray8 format ;-)

---
+++ scailx_tools/boson/gst_boson_640x512_NV12_Gray8_yolo.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+gst-launch-1.0 \
+  v4l2src device=/dev/video0 ! video/x-raw,format=NV12,width=640,height=512,framerate=60/1 ! \
+  videorate max-rate=10 ! video/x-raw,framerate=10/1 ! \
+  queue leaky=2 max-size-buffers=10 ! videoconvert ! video/x-raw,format=GRAY8 ! \
+  tee name=t \
+  t. ! queue leaky=2 max-size-buffers=10 ! \
+  videoscale method=0 ! video/x-raw,width=320,height=320 ! \
+  videoconvert ! video/x-raw,format=RGB ! \
+  tensor_converter ! \
+  tensor_transform mode=arithmetic option=typecast:float32,add:0.0,div:255.0 ! \
+  queue leaky=2 max-size-buffers=10 ! \
+  tensor_filter latency=1 framework=tensorflow2-lite model=/opt/imx8-isp/boson/yolov8n_float16.tflite ! \
+  tensor_transform mode=transpose option=1:0:2:3 ! \
+  queue leaky=2 max-size-buffers=10 ! \
+  tensor_decoder mode=bounding_boxes option1=yolov8 option2=/opt/imx8-isp/boson/coco.txt option4=320:320 option5=320:320 ! \
+  videoconvert ! queue leaky=2 max-size-buffers=10 ! \
+  videoscale method=0 ! video/x-raw,width=640,height=512 ! mix.sink_0 \
+  t. ! queue leaky=2 max-size-buffers=10 ! videoconvert ! mix.sink_1 \
+  compositor name=mix sink_0::zorder=2 sink_1::zorder=1 ! videoconvert ! autovideosink sync=false
