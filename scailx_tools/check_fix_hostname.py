8f911684ad8c4ded3747fafeab76868370fb7fc3
PingVideology <jye@videologyinc.com>
2026-04-01 15:40:53 -0400

    AB#2121. Added pydoc comments to 5 py files ;-)

--- scailx_tools/check_fix_hostname.py
+++ scailx_tools/check_fix_hostname.py
@@ -34,8 +34,19 @@ import socket
 import uuid
 import re
 
+
 # Get MAC address from local device and return as 00:0C:C6:0E:9A:92.
 def get_mac_address():
+    """
+    Get MAC address from local device and return as string like 00:0C:C6:0E:9A:92.
+
+    Arguments:
+
+    Returns:
+    str --  MAC address in standard format string like 00:0C:C6:0E:9A:92. Or empty if error or not found.
+
+    """
+
     # Get the MAC address as a 48-bit integer
     mac_num = uuid.getnode()
 
@@ -46,12 +57,25 @@ def get_mac_address():
         return ""
 
     # Format the integer into a standard hex string with colons
-    hex_mac = str(":".join(re.findall('..', '%012x' % mac_num)))
+    hex_mac = str(":".join(re.findall("..", "%012x" % mac_num)))
     return hex_mac.upper()
+
 
 # Create mac based hostname like scalix-AABBCC
 def get_mac_hostname(prefix, mac):
-    if len(mac) !=17:
+    """
+    Given input user specified prefix and MAC address string, create mac based hostname like sca-AABBCC as return.
+
+    Arguments:
+    prefix  --  User input prefix such as scalix or sca. Recommented length >=3.
+    mac     --  Standard format MAC address string from function get_mac_hostname().
+
+    Returns:
+    str --  Generated new hostname in the form of prefix + "-" + 6 chars of last 3 hex strings of MAC address.
+
+    """
+
+    if len(mac) != 17:
         return ""
     # Use last 6 chars 9:10 + 12:13 + 14:15
     mac_hostname = prefix + "-" + mac[9:11] + mac[12:14] + mac[15:]
@@ -60,6 +84,16 @@ def get_mac_hostname(prefix, mac):
 
 # Run hostname command to get local stored hostname
 def run_hostname():
+    """
+    Run hostname command to get local stored hostname as return.
+
+    Arguments:
+
+    Returns:
+    str --  Hostname returned by 'hostname' command.
+
+    """
+
     result = subprocess.run(
         ["hostname"],
         capture_output=True,
@@ -68,8 +102,22 @@ def run_hostname():
     res = result.stdout
     return res
 
-# Run avahi command to get posisble hostname conflict.
+
+# Run avahi command to get possible hostname conflict.
 def run_avahi():
+    """
+    Run avahi command to get possible hostname conflict.
+
+    Arguments:
+
+    Returns:
+    (bool, str) --  Bool flag = True if conflict detected, and detected hostname by avahi.
+
+    Notes:
+    The detected hostname by avahi and by hostname commands may be different sometimes before rebooting the scailx device.
+
+    """
+
     result = subprocess.run(
         ["systemctl", "status", "avahi-daemon", "|", "grep", "-i", '"Host name"'],
         capture_output=True,
@@ -96,8 +144,19 @@ def run_avahi():
         return False, hostname
 
 
-# Set hostname by avahi output.
+# Set new hostname using hostnamectl command.
 def fix_hostname(hostname):
+    """
+    Given input new hostname, set hostname by hostnamectl command.
+
+    Arguments:
+    hostname  --  User input new hostname to change into.
+
+    Returns:
+    str --  Output message from hostnamectl command.
+
+    """
+
     # set hostname
     res1 = subprocess.run(["hostnamectl", "set-hostname", hostname])
 
@@ -110,6 +169,18 @@ def fix_hostname(hostname):
 
 
 def check_hostname_exists(user_hostname):
+    """
+    Given input new hostname, check whether it already exists on the same network.
+    Return bool flag and IP address to access.
+
+    Arguments:
+    user_hostname  --  User input new hostname to change into.
+
+    Returns:
+    (bool, str) --  Bool flag = True if hostname exists, and its IP address. Or = False if it does not exist, and empty string.
+
+    """
+
     try:
         # Attempt to resolve the hostname to an IPv4 address
         ip_address = socket.gethostbyname(user_hostname)
@@ -121,6 +192,18 @@ def check_hostname_exists(user_hostname):
 
 # Change to new hostname
 def change_hostname(user_hostname):
+    """
+    Given input new hostname, check whether it already exists on the same network. Change to new hostname if it does not exist.
+    Return exists or hostnamectl command output messages.
+
+    Arguments:
+    user_hostname  --  User input new hostname to change into.
+
+    Returns:
+    str --  Warning message if hostname already exists. Or hostnamectl command output if it is changed successfully.
+
+    """
+
     # First need to check this new hostname unoccupied.
     res, ip_address = check_hostname_exists(user_hostname)
     if res:
@@ -138,6 +221,27 @@ def change_hostname(user_hostname):
 
 
 if __name__ == "__main__":
+    """
+    check_fix_hostname.py main() function.
+
+    With user inputs by argmenents, the program can just show hostname + IP address and whether it has conflicts.
+    Or change it by avahi suggestion automatically.
+    Or force changing it by user input.
+    Or force changing it by prefix + "-" + MAC address last 6 chars.
+
+    Usage examples:
+
+    python3 check_fix_hostname.py -h    (See help.)
+
+    python3 check_fix_hostname.py       (Just show conflict and hosname.)
+
+    python3 check_fix_hostname.py -i 1     (Show conflict, hosname, IP addres and MAC address.)
+
+    python3 check_fix_hostname.py --mac 1    (Change hostname using MAC address last 6 chars such as scailx-0E9A92. Reboot to be effective.)
+
+    python3 check_fix_hostname.py -i 1 -f 1   (Show conflict and fix it by avahi. Reboot scailx to be effective.)
+
+    """
 
     parser = argparse.ArgumentParser(
         description="Check hostname conflict by avahi and fix it",
@@ -154,7 +258,10 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        "--mac", type=int, default=0, help="Change hostname using last 6 digits of MAC address"
+        "--mac",
+        type=int,
+        default=0,
+        help="Change hostname using last 6 digits of MAC address",
     )
 
     parser.add_argument(
@@ -184,13 +291,15 @@ if __name__ == "__main__":
 
     # Force change host name using mac to scailx-AABBCC
     mac_hostname = ""
-    if args.mac and mac !="":
+    if args.mac and mac != "":
         mac_hostname = get_mac_hostname(args.prefix, mac)
 
     # New hostname format example = 	sca-0E9A92	(total 10 chars)
     if len(mac_hostname) >= 10:
         res = change_hostname(mac_hostname)
-        print("hostname of this device is changed. Please remember it and use it to access for ssh and on web / VLC player after reboot.")
+        print(
+            "hostname of this device is changed. Please remember it and use it to access for ssh and on web / VLC player after reboot."
+        )
         print(res)
     # Force manual fix.
     elif args.fix and len(args.manualhostname) >= 10:
@@ -203,4 +312,3 @@ if __name__ == "__main__":
         print(res)
     else:
         print("No hostname conflict. No need to fix anything ;-)")
-    
--- scailx_tools/detect_cameras_live.py
+++ scailx_tools/detect_cameras_live.py
@@ -23,14 +23,16 @@ from pathlib import Path
 import io
 import warnings
 
-warnings.filterwarnings('ignore')
-os.environ['OPENCV_LOG_LEVEL'] = 'OFF'
-os.environ['GST_DEBUG'] = '*:0' 
+warnings.filterwarnings("ignore")
+os.environ["OPENCV_LOG_LEVEL"] = "OFF"
+os.environ["GST_DEBUG"] = "*:0"
 
 import cv2
 
 # Maximum camera index with /dev/video* to detect.
-MAX_CAMERA_ID = 3
+MAX_CAMERA_ID = 10
+""" Maximum camera index with /dev/video* to detect. """
+
 
 # Camera key words in device tree and its regular names
 camera_dict = {
@@ -40,13 +42,26 @@ camera_dict = {
     "imx900": "imx900",
     "imx678": "imx678",
     "imx662": "imx662",
-    "usb" : "usb",
+    "usb": "usb",
 }
+""" Camera key words in device tree and its regular names. """
+
 
 # Check camera on/off using OpenCV VideoCapture().
 def check_camera(device_path="/dev/video0"):
+    """
+    Given input camera device path, check camera on/off using OpenCV VideoCapture().
+
+    Arguments:
+    device_path  --  Camera device path such as /dev/video0, etc.
+
+    Returns:
+    bool --  True if camera is ready to start streaming. False if camera is busy or cannot open.
+
+    """
+
     cap = cv2.VideoCapture(device_path)
-    
+
     # Check if the camera is opened successfully
     if cap.isOpened():
         ret, frame = cap.read()
@@ -56,34 +71,70 @@ def check_camera(device_path="/dev/video0"):
         else:
             return False
     else:
-    # print("Camera is OFF or not accessible.")
+        # print("Camera is OFF or not accessible.")
         return False
+
 
 # Check whether camera path is available.
 def is_device_available(device="/dev/video0"):
+    """
+    Given input camera device path, check whether camera path is available.
+
+    Arguments:
+    device_path  --  Camera device path such as /dev/video0, etc.
+
+    Returns:
+    bool --  True if camera path exists. False if it does not and cannot be opened.
+
+    """
+
     if os.path.exists(device):
         return True
     else:
         return False
 
+
 # Given camera name in device tree folder, find its real device path.
 def devicetree_cam_to_path(camfile):
+    """
+    Given camera name in device tree folder, find its real device path as return.
+
+    Arguments:
+    camfile  --  Camera device tree full path such as /proc/device-tree/chosen/overlays/cam*.
+
+    Returns:
+    str --  Camera device path like /dev/video-isp-csi0, etc.
+
+    """
+
     cam = os.path.basename(camfile)
     camlist = re.findall(r"cam(\d+)-(\w+)", cam)
-    if len(camlist)==0:
+    if len(camlist) == 0:
         return ""
     idn, typ = camlist[0]
     devlist = glob.glob(f"/dev/video*csi{idn}")
-    if len(devlist)==0:
+    if len(devlist) == 0:
         return ""
     vdev = devlist[0]
     cam_real_path = Path(vdev).resolve()
     return str(cam_real_path)
-    
+
 
 # Use system chosen device tree name to detect camera type:   ar0234, imx, boson, or ZoomBlock "lvds".
 # Return tuple of (camera name, device tree name), ("unknown", "unknown") or ("", "").
 def detect_camera_type(device="/dev/video0"):
+    """
+    This is reverse function of devicetree_cam_to_path().
+    Given camera device path such as /dev/video0, find its device tree path or v4l path for usb cameras as return.
+
+    Arguments:
+    device  --  Camera device path such as /dev/video0.
+
+    Returns:
+    (str, str) --  Camera normal name in dict, and its matching device tree name or v4l by-path name for usb cameras. Or (empty, empty) if not found.
+
+    """
+
     if is_device_available(device) and ("/dev/video" in device):
         # Find matching device tree name id.
         cam_list = glob.iglob("/proc/device-tree/chosen/overlays/cam*")
@@ -116,6 +167,16 @@ def detect_camera_type(device="/dev/video0"):
 
 # Detect cameras with /dev/video* and return dict with camera_path : (camera_name, devicetree_name, on/off).
 def detect_cameras():
+    """
+    Detect cameras with /dev/video* and return dict with camera_path : (camera_name, devicetree_name, on/off).
+
+    Arguments:
+
+    Returns:
+    dict[str: (str,str,bool)] --  dict of key = camera name, value = camera status tuple = (camera name, device tree or v4l name, is ready for streaming).
+
+    """
+
     camera_status = {}
     prefix = "/dev/video"
     for id in range(0, MAX_CAMERA_ID):
@@ -130,8 +191,22 @@ def detect_cameras():
 
 
 def restart_go2rtc():
+    """
+    Restart go2rtc.service running subprocess.run() and outputs messages.
+
+    Arguments:
+
+    Returns:
+
+    """
+
     try:
-        result = subprocess.run(["systemctl", "restart", "go2rtc.service"], capture_output=True, text=True, check=True)
+        result = subprocess.run(
+            ["systemctl", "restart", "go2rtc.service"],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
         print("Restart go2rtc: ", result.stdout)
     except Exception as e:
         print(f"Restart go2rtc failed: {e}")
@@ -139,6 +214,22 @@ def restart_go2rtc():
 
 # Given initial camera status dict, use infinite loopt to check camera status every few seconds.
 def monitor_cameras_loop(camera_status, interval):
+    """
+    Given initial camera status dict, use infinite loop to check camera status every few seconds.
+    Restart go2rtc.service if camera number of items in ths dict changed.
+
+    Arguments:
+    camera_status  --   dict of camera status from function detect_cameras().
+    interval       --   Interval in seconds to sleep between each camera status check. = 5 seconds by default.
+
+    Returns:
+
+    Notes:
+    If usb camera is connected or dis-connected, camera number of items will increase or decrease.
+    go2rtc.service will restart and camera config yaml is re-generated, which will make webRTC port 1984 GUI stream list change accordingly.
+
+    """
+
     nitems = len(camera_status)
     while True:
         current_camera_status = detect_cameras()
@@ -157,6 +248,12 @@ def monitor_cameras_loop(camera_status, interval):
 
 
 if __name__ == "__main__":
+    """
+    detect_camera_live.py.
+
+    With user input argument of interval gap in seconds, the program loops to check camera status regularly and restart go2rtc.service if number of items changed.
+
+    """
 
     parser = argparse.ArgumentParser(
         description="Detect cameras live",
