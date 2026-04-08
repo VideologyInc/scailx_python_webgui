93fd010077e135ff71f405ed3a69f51f58586c9a
PingVideology <jye@videologyinc.com>
2026-04-08 12:55:06 -0400

    AB#2127. Added 2 imx parameter json file and related json read in py.

--- scailx_tools/detect_imx_live.py
+++ scailx_tools/detect_imx_live.py
@@ -4,6 +4,7 @@
 
 File:   detect_imx_live.py
 
+2026.0408.  Added json file to 'WINDOW NAME' : 'values' parameter dict parsing to set user specified values.
 2026.0406.  Detect one imx camera live stream on/off status using lsof and let user choose to Reset AEC and/or AWB with optimized values.
 
 By:			jye@videologyinc.com
@@ -19,6 +20,8 @@ import sys
 import time
 
 from vdlg_lvds.detect_cameras_live import detect_camera_type
+from vdlg_lvds.read_imx_json import read_imx, default_imx
+
 
 # Given jc parsed lsof output list of dict, check command containing isp_media (imx is available) or gst-launc (imx stream on).
 # return (bool, bool) of (camera on, stream on)
@@ -26,10 +29,10 @@ def check_lsof_output(data_list_dict):
     camera_on = False
     stream_on = False
     for d in data_list_dict:
-        if d["command"]=="isp_media":
-            camera_on =True
-        elif d["command"]=="gst-launc":
-            stream_on =True
+        if d["command"] == "isp_media":
+            camera_on = True
+        elif d["command"] == "gst-launc":
+            stream_on = True
 
     return camera_on, stream_on
 
@@ -44,15 +47,15 @@ def detect_imx(device_path):
         cmd_output = subprocess.check_output(cmd, shell=True, text=True)
 
         # Parse into a list of dictionaries
-        data_list_dict = jc.parse('lsof', cmd_output)
+        data_list_dict = jc.parse("lsof", cmd_output)
         status = check_lsof_output(data_list_dict)
         # print(data)
         return status
-        
+
     except subprocess.CalledProcessError:
         print("Device not found or error running lsof")
         return False, False
-    
+
 
 def restart_go2rtc():
 
@@ -66,6 +69,7 @@ def restart_go2rtc():
         print("Restart go2rtc: ", result.stdout)
     except Exception as e:
         print(f"Restart go2rtc failed: {e}")
+
 
 # Turn AEC or AWB off.
 def vvget_set_feature_off(camera_id, feature_name, value_name="", value=""):
@@ -99,20 +103,15 @@ def vvget_set_feature_off(camera_id, feature_name, value_name="", value=""):
         text=True,
         check=True,
     )
-    # Get value
-    if value_name:
-        if value:
-            # 3.1. Set value first
-            cmd3 = f"vvget {camera_id} '{value_name}' '{value}'"
-            result3 = subprocess.run(
-                cmd3,
-                shell=True,
-                capture_output=True,
-                text=True,
-                check=True,
-            )
-        # 3.2 Get value
-        cmd3 = f"vvget {camera_id} '{value_name}'"
+    return result2.stdout
+
+
+# Set values by para dict.
+def vvget_set_feature_values(camera_id, imx_para):
+
+    for key, value in imx_para.items():
+        # 3.1. Set value first
+        cmd3 = f"vvget {camera_id} '{key}' '{value}'"
         result3 = subprocess.run(
             cmd3,
             shell=True,
@@ -120,11 +119,23 @@ def vvget_set_feature_off(camera_id, feature_name, value_name="", value=""):
             text=True,
             check=True,
         )
-        return result2.stdout + result3.stdout
-    return result2.stdout
+
+    message = ""
+    for key, value in imx_para.items():
+        # 3.2 Get value
+        cmd3 = f"vvget {camera_id} '{key}'"
+        result3 = subprocess.run(
+            cmd3,
+            shell=True,
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+        message += result3.stdout
+    return message
 
 
-def turn_off_aec_awb(device_path, aec=True, awb=True):
+def turn_off_aec_awb(device_path, imx_para, aec=True, awb=True):
     dlen = len("/dev/video")
     # Do nothing if device path is not /dev/video?
     if len(device_path) <= dlen:
@@ -135,22 +146,32 @@ def turn_off_aec_awb(device_path, aec=True, awb=True):
     try:
         # Reset AEC with optimized exposure time value.
         if aec:
-            print("vvget AEC Status: ", vvget_set_feature_off(id, "AEC", "AEC ExposureTime", "0.00957"))
+            print("vvget AEC Status: ", vvget_set_feature_off(id, "AEC"))
         # Reset AWB with optimized gain input values.
         if awb:
-            print("vvget AWB Off: ", vvget_set_feature_off(id, "AWB", "GAIN INPUT", "1.1, 1, 1, 3.0"))
+            print("vvget AWB Status: ", vvget_set_feature_off(id, "AWB"))
+
+        print(vvget_set_feature_values(id, imx_para))
+
     except Exception as e:
         print(f"vvget failed: {e}")
 
 
 # Given initial camera status, use infinite loopt to check camera stream status every few seconds.
-def monitor_cameras_loop(device_path, camera_status, stream_status, interval, aec=True, awb=True):
+def monitor_cameras_loop(
+    device_path, camera_status, stream_status, interval, imx_para, aec=True, awb=True
+):
 
     try:
         while True:
             current_camera_status, current_stream_status = detect_imx(device_path)
-            if current_camera_status == camera_status and current_stream_status==stream_status:
-                print(f"Camera {current_camera_status} and stream {current_stream_status}")
+            if (
+                current_camera_status == camera_status
+                and current_stream_status == stream_status
+            ):
+                print(
+                    f"Camera {current_camera_status} and stream {current_stream_status}"
+                )
             elif current_camera_status != camera_status:
                 print("Camera status changed: ", current_camera_status)
                 # update status for next time check.
@@ -164,12 +185,13 @@ def monitor_cameras_loop(device_path, camera_status, stream_status, interval, ae
                 stream_status = current_stream_status
                 # Off => On change: need to turn off AEC and AWB
                 if current_stream_status:
-                    turn_off_aec_awb(device_path, aec, awb)
+                    turn_off_aec_awb(device_path, imx_para, aec, awb)
 
-            # sleep a few seconds for next check.    
+            # sleep a few seconds for next check.
             time.sleep(interval)
     except KeyboardInterrupt:
         print("\nLoop stopped by user.")
+
 
 if __name__ == "__main__":
 
@@ -181,12 +203,24 @@ if __name__ == "__main__":
         "-d", "--device", type=str, default="/dev/video0", help="camera device path"
     )
     parser.add_argument(
-        "--aec", type=int, default=1, help="Turn Off and Reset AEC with optimized exposure time value."
+        "--aec",
+        type=int,
+        default=1,
+        help="Turn Off and Reset AEC with optimized exposure time value.",
     )
     parser.add_argument(
-        "--awb", type=int, default=1, help="Turn Off and Reset AWB with optimized gain input values."
+        "--awb",
+        type=int,
+        default=1,
+        help="Turn Off and Reset AWB with optimized gain input values.",
     )
-
+    parser.add_argument(
+        "-j",
+        "--json",
+        type=str,
+        default="",
+        help="Read Window Name : values parameter dict from a json file.",
+    )
 
     parser.add_argument(
         "-i",
@@ -205,8 +239,20 @@ if __name__ == "__main__":
         print(f"Camera at {args.device} is not a Sony imx camera.")
         sys.exit()
 
+    imx_para = (
+        read_imx(args.json) if args.json.endswith(".json") else default_imx(camera_type)
+    )
+
     # Get current camera status
     camera_status, stream_status = detect_imx(args.device)
     print(f"Initial camera status: camera {camera_status}, stream {stream_status}")
 
-    monitor_cameras_loop(args.device, camera_status, stream_status, args.interval, args.aec, args.awb)
+    monitor_cameras_loop(
+        args.device,
+        camera_status,
+        stream_status,
+        args.interval,
+        imx_para,
+        args.aec,
+        args.awb,
+    )
---
+++ scailx_tools/read_imx_json.py
@@ -0,0 +1,109 @@
+"""
+
+read_imx_json.py
+
+Read imx parameters 'Window Name' and their values from a json file.
+
+"""
+
+import json
+
+IMX900_PARA = {
+    "AEC GAIN": "40.0",
+    "AEC ExposureTime": "0.00957",
+    "AEC Sensitivity": "100",
+    "GAIN INPUT": "1.32,1.0,1.0,3.0",
+    "Offset INPUT": "-29,-32,-33",
+    "CPROC ON/OFF": "1",
+    "Adjust brightness": "0",
+    "Adjust contrast": "1.2",
+    "Adjust saturation": "1.0",
+    "Adjust HUE": "0",
+}
+
+IMX678_PARA = {
+    "AEC GAIN": "40.0",
+    "AEC ExposureTime": "0.03072",
+    "AEC Sensitivity": "100",
+    "GAIN INPUT": "1.22,1.0,1.0,2.83",
+    "Offset INPUT": "-36,-38,-29",
+    "CPROC ON/OFF": "1",
+    "Adjust brightness": "0",
+    "Adjust contrast": "1.2",
+    "Adjust saturation": "1.0",
+    "Adjust HUE": "0",
+}
+
+DEFAULT_PARA = {
+    "AEC GAIN": "40.0",
+    "AEC ExposureTime": "0.009",
+    "AEC Sensitivity": "100",
+    "GAIN INPUT": "1.3,1.0,1.0,2.83",
+    "Offset INPUT": "-30,-30,-30",
+    "CPROC ON/OFF": "1",
+    "Adjust brightness": "0",
+    "Adjust contrast": "1.0",
+    "Adjust saturation": "1.0",
+    "Adjust HUE": "0",
+}
+
+GRAY_PARA = {
+    "AEC GAIN": "40.0",
+    "AEC ExposureTime": "0.009",
+    "AEC Sensitivity": "100",
+    "GAIN INPUT": "1.0,1.0,1.0,1.0",
+    "Offset INPUT": "-30,-30,-30",
+    "CPROC ON/OFF": "1",
+    "Adjust brightness": "0",
+    "Adjust contrast": "1.0",
+    "Adjust saturation": "0.0",
+    "Adjust HUE": "0",
+}
+
+
+# Set default parameters if json not found or read error.
+def default_imx(camera_type):
+    if camera_type == "imx900":
+        return IMX900_PARA
+    elif camera_type == "imx678":
+        return IMX678_PARA
+    elif camera_type == "imxgray":
+        return GRAY_PARA
+    else:
+        return DEFAULT_PARA
+
+
+# Parse data dict from json file.
+def parse_imx(data_dict):
+
+    return data_dict
+
+
+def read_imx(filename):
+
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            data_dict = json.load(
+                file
+            )  # Deserialize the file data into a Python dictionary
+            # print(data_dict)
+
+            para_dict = parse_imx(data_dict)
+
+            return para_dict
+
+    except FileNotFoundError:
+        print(f"Error: The file '{filename}' was not found.")
+        return {}
+    except json.JSONDecodeError:
+        print(f"Error: Could not decode JSON from the file. Check file format.")
+        return {}
+
+    return {}
+
+
+# imx900 = read_imx("imx900.json")
+# print(imx900)
+
+# imx678 = read_imx("imx678.json")
+# print(imx678)
